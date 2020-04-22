e681c54029a85bcfb342b98f275edcf485292d36
RanjitVarun <ranjit.gaudaman@aspiresys.com>
2020-04-22 18:36:28 +0530

    navbar removed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Route, Router } from 'react-router-dom'
 
 import client from './components/Client'
-import navbar from './components/Navbar'
+
 import appservice from './components/AppService'
 import subscriptions from './components/ClientSubscription'
 class App extends React.Component {
@@ -13,7 +13,7 @@ class App extends React.Component {
                 <Route path="/" exact component={client}></Route>
                 <Route path="/appservice" exact component={appservice}></Route>
                 <Route path="/subscriptions" exact component={subscriptions}></Route>
-                <Route path="/navbar" exact component={navbar}></Route>
+               
               
             </div>
         );
